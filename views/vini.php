a8731144c9c017bd1cb0611b23cbb7c2217f9c26
katerperez <katerinamarillo053@gmail.com>
2025-03-19 15:24:13 -0500

    TIENE RESPONSIVE

--- views/vini.php
+++ views/vini.php
@@ -1,29 +1,32 @@
-<div class="row border border-black inier row border border-black">
-    <form action="models/validacion.php" method="POST">
-        <h1 class="tbl">Iniciar Sesión</h1>
-        <div class="input-group">
-            <div class="ini">
-                <i class="fa-solid fa-envelope"></i>
-                <input class="inl" name="usu" type="text" placeholder="Correo Electrónico" required>
-            </div>
-            <div class="ini">
-                <i class="fa-solid fa-lock"></i>
-                <input class="inl" name="con" type="password" placeholder="Contraseña" required>
-            </div>
-            <div class="d-flex justify-content-between">
-                <button id="btn-abm" class="btn btn-secondary me-2" type="submit">Ingreso</button>
-                <a href="index.php?pg=205">
-                    <button id="btn-abm" class="btn btn-secondary ms-2" type="button">Olvido su contraseña</button>
-                </a>
-            </div> 
+<div class="container py-5 mt-5">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-6 col-lg-4 border border-black rounded-4 p-4 shadow">
+            <form action="models/validacion.php" method="POST">
+                <h1 class="text-center mb-4 tbl">Iniciar Sesión</h1>
+                <!-- Espaciado más amplio -->
+                <div class="input-group mb-3 px-5">
+                    <div class="ini w-100 mb-3">
+                        <i class="fa-solid fa-envelope me-2"></i>
+                        <input class="form-control inl" name="usu" type="text" placeholder="Correo Electrónico" required>
+                    </div>
+                    <div class="ini w-100 mb-3">
+                        <i class="fa-solid fa-lock me-2"></i>
+                        <input class="form-control inl" name="con" type="password" placeholder="Contraseña" required>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-between px-5">
+                        <button class="btn btn-secondary custom-btn-size me-2" type="submit">Ingreso</button>
+                        <a href="index.php?pg=205" class="btn btn-secondary custom-btn-size ms-2 text-center">Olvidó su contraseña</a>
+                </div>
+
+                <!-- Mensaje de error -->
+                <?php 
+                $error = isset($_GET['error']) ? $_GET['error'] : NULL; 
+                if ($error == "ok") { 
+                    echo "<div class='alert alert-danger mt-3'>Datos inválidos. Vuelve a intentarlo.</div>"; 
+                }
+                ?>
+            </form>
         </div>
-        <?php 
-            $error = isset($_GET['error']) ? $_GET['error'] : NULL; 
-            if ($error == "ok") { 
-                echo "<div class='alert alert-danger'>Datos inválidos. Vuelve a intentarlo.</div>"; 
-            }
-        ?>
-    </form>
+    </div>
 </div>
-
-
