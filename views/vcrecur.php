956689525889584667db90d43402b6a4551de1ac
katerperez <katerinamarillo053@gmail.com>
2025-03-21 17:29:49 -0500

    ..

--- views/vcrecur.php
+++ views/vcrecur.php
@@ -11,49 +11,58 @@
             </div>
         </div>
     </div>
-    <form name="frm1" action="home.php?pg=<?= $pg; ?>" method="POST" class="toggleForm">
-    <div class="row g-3 p-3">
-        <div class="form-group col-12 col-md-6">
-            <label for="codcur">Número del Curso</label>
-            <input type="text" class="form-control" name="codcur" value="<?php echo isset($datOne[0]['codcur']) ? $datOne[0]['codcur'] : ''; ?>" required>
+    <form name="frm1" action="home.php?pg=<?= $pg; ?>" method="POST" class="toggleForm" style="display:none;">
+        <div class="row g-3 p-3">
+            <div class="form-group col-12 col-md-6">
+                <label for="codcur">Número del Curso</label>
+                <input type="text" class="form-control" name="codcur" value="<?php echo isset($datOne[0]['codcur']) ? $datOne[0]['codcur'] : ''; ?>" required>
+            </div>
+            <div class="form-group col-12 col-md-6">
+                <label for="nomcur">Nombre del Curso</label>
+                <input type="text" class="form-control" name="nomcur" value="<?php echo isset($datOne[0]['nomcur']) ? $datOne[0]['nomcur'] : ''; ?>" required>
+            </div>
+            <input type="hidden" name="idcur" value="<?php echo isset($datOne[0]['idcur']) ? $datOne[0]['idcur'] : ''; ?>">
+            <input type="hidden" name="ope" value="save">
+            <div class="col-12 col-md-2">
+                <input class="btn btn-primary mt-4" type="submit" value="Guardar">
+            </div>
         </div>
-        <div class="form-group col-12 col-md-6">
-            <label for="nomcur">Nombre del Curso</label>
-            <input type="text" class="form-control" name="nomcur" value="<?php echo isset($datOne[0]['nomcur']) ? $datOne[0]['nomcur'] : ''; ?>" required>
-        </div>
-        <input type="hidden" name="idcur" value="<?php echo isset($datOne[0]['idcur']) ? $datOne[0]['idcur'] : ''; ?>">
-        <input type="hidden" name="ope" value="save">
-        <div class="col-12 col-md-2">
-            <input class="btn btn-primary mt-4" type="submit" value="Guardar">
-        </div>
-    </div>
     </form>
 
-</div>
-<div class="table-responsive">
-    <table id="example" class="table table-striped table-hover">
-        <thead class="table-dark">
-            <tr>
-                <th>Nombre</th>
-                <th>Número de curso</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php if ($datAll) { foreach ($datAll as $dta) { ?>
+    <div class="table-responsive">
+        <table id="example" class="table table-striped table-hover">
+            <thead class="table-dark">
                 <tr>
-                    <td><strong><?= $dta['codcur']; ?></strong></td>
-                    <td><strong><?= $dta['nomcur']; ?></strong></td>
-                    <td class="text-end">
-                        <a href="home.php?pg=<?= $pg; ?>&idcur=<?= $dta['idcur']; ?>&ope=edi" title="Editar">
-                            <i class="fa-solid fa-pen-to-square fa-2x"></i></a>
-                        <a href="home.php?pg=<?= $pg; ?>&idcur=<?= $dta['idcur']; ?>&ope=eli" title="Eliminar" onclick="return eliminar();">
-                            <i class="fa-solid fa-trash-can fa-2x"></i></a>
-                    </td>
+                    <th>Número de curso</th>
+                    <th>Nombre del Curso</th>
+                    <th>Acciones</th>
                 </tr>
-            <?php }} ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php if ($datAll) { foreach ($datAll as $dta) { ?>
+                    <tr>
+                        <td><?= $dta['codcur']; ?></td>
+                        <td><?= $dta['nomcur']; ?></td>
+                        <td class="text-end">
+                            <a href="home.php?pg=<?= $pg; ?>&idcur=<?= $dta['idcur']; ?>&ope=edi" title="Editar">
+                                <i class="fa-solid fa-pen-to-square"></i>
+                            </a>
+                            <a href="home.php?pg=<?= $pg; ?>&idcur=<?= $dta['idcur']; ?>&ope=eli" title="Eliminar" onclick="return eliminar();">
+                                <i class="fa-solid fa-trash-can"></i>
+                            </a>
+                            <a href="home.php?pg=lisidcur=<?= $dta['idcur']; ?>" title="Listar estudiantes">
+                                <i class="fa-solid fa-users"></i>
+                            </a>
+                        </td>
+                    </tr>
+                <?php }} else { ?>
+                    <tr>
+                        <td colspan="3" class="text-center">No hay cursos disponibles</td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
 </div>
 
 <script type="text/javascript" src="js/java2.js"></script>
