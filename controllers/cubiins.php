dc5f8e0d1f3f9c996e6ec44fda520869c360eb38
WagnerA0 <alejo.wagner03@gmail.com>
2025-03-26 14:33:45 -0500

    Create cubiins.php

---
+++ controllers/cubiins.php
@@ -0,0 +1,40 @@
+<?php
+	require_once('models/conexion.php');
+
+	$valor = $_REQUEST["valor"];
+	$data=selmuni($valor);
+	//echo $sql;
+	$i=0;
+	foreach ($data as $res){
+		$mmun[$i]["value"]=$res["codubi"];
+		$mmun[$i]["nombre"]=$res["nomubi"];
+		$i++;
+	}
+	$html='<div id="reloadMun"><select name="ubiest" id="id_estado" class="form-control">';
+	$html.='<option value="">seleccione</option>';
+	
+	foreach ($mmun as $res){
+			$html.='<option value="'.$res["value"].'">'.$res["nombre"].'</option>';
+		}
+
+	$html.='</select></div>';
+	echo $html;	
+
+
+	function selmuni($valor){
+		$resultado=null;
+		$modelo=new conexion();
+		$conexion=$modelo->get_conexion();
+		$sql="SELECT codubi, nomubi, depubi FROM ubica WHERE depubi=:valor";
+		//echo $sql;
+		$result=$conexion->prepare($sql);
+
+		$result->bindParam(':valor', $valor);
+		$result->execute();
+
+		while($f=$result->fetch()){
+			$resultado[]=$f;
+		}
+		return $resultado;
+	}
+?>
