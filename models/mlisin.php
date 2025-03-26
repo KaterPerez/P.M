bcebe9135f92207c2c383eb89af1d651b7121ea5
WagnerA0 <alejo.wagner03@gmail.com>
2025-03-26 14:26:59 -0500

    Update mlisin.php

--- models/mlisin.php
+++ models/mlisin.php
@@ -1,126 +1,220 @@
-<?php
-class Mlisin {
-  private $nuicie;
-  private $depubi;
-  private $munubi;
-  private $corie;
-  private $telie;
+<?php class mlisin
+{
 
-  // Métodos Getter
-  public function getNuicie() {
-    return $this->nuicie;
-  }
+    //atributos
+    private $codie;
+    private $nomie;
+    private $tipie;
+    private $dirie;
+    private $nuicie;
+    private $corie;
+    private $codubi;
+    private $telie;
+    private $actie;
 
-  public function getDepubi() {
-    return $this->depubi;
-  }
+    //metodos get
+    public function getCodie()
+    {
+        return $this->codie;
+    }
+    public function getNomie()
+    {
+        return $this->nomie;
+    }
+    public function getTipie()
+    {
+        return $this->tipie;
+    }
+    public function getDirie()
+    {
+        return $this->dirie;
+    }
+    public function getNuicie()
+    {
+        return $this->nuicie;
+    }
+    public function getCorie()
+    {
+        return $this->corie;
+    }
+    public function getCodubi()
+    {
+        return $this->codubi;
+    }
+    public function getTelie()
+    {
+        return $this->telie;
+    }
+    public function getActie()
+    {
+        return $this->actie;
+    }
+   
 
-  public function getMunubi() {
-    return $this->munubi;
-  }
+    //metodos SET_
+    public function setCodie($codie)
+    {
+        $this->codie = $codie;
+    }
+    public function setNomie($nomie)
+    {
+        $this->nomie = $nomie;
+    }
+    public function setTipie($tipie)
+    {
+        $this->tipie = $tipie;
+    }
+    public function setDirie($dirie)
+    {
+        $this->dirie = $dirie;
+    }
+    public function setNuicie($nuicie)
+    {
+        $this->nuicie = $nuicie;
+    }
+    public function setCorie($corie)
+    {
+        $this->corie = $corie;
+    }
+    public function setCodubi($codubi)
+    {
+        $this->codubi = $codubi;
+    }
+    public function setTelie($telie)
+    {
+        $this->telie = $telie;
+    }
+    public function setActie($actie)
+    {
+        $this->actie = $actie;
+    }
 
-  public function getCorie() {
-    return $this->corie;
-  }
+    public function getAll()
+    {
+        $res = NULL;
+        $sql = "SELECT codie, nomie, tipie, dirie, nuicie, corie, codubi, telie, actie FROM ie";
+        $modelo = new Conexion();
+        $conexion = $modelo->get_conexion();
+        $result = $conexion->prepare($sql);
+        $result->execute();
+        $res = $result->fetchAll(PDO::FETCH_ASSOC);
+        return $res;
+    }
+    public function getOne()
+    {
+        $res = NULL;
+        $sql = "SELECT codie, nomie, tipie, dirie, nuicie, corie, codubi, telie, actie FROM ie WHERE codie=:codie";
+        $modelo = new Conexion();
+        $conexion = $modelo->get_conexion();
+        $result = $conexion->prepare($sql);
+        $codie = $this->getCodie();
+        $result->bindParam(":codie", $codie);
+        $result->execute();
+        $res = $result->fetchall(PDO::FETCH_ASSOC);
+        return $res;
+    }
+    public function save()
+    {
+        $modelo = new Conexion();
+        $conexion = $modelo->get_conexion();
 
-  public function getTelie() {
-    return $this->telie;
-  }
+        // Generar un código único para `codie`
+        do {
+            $codie = str_pad(rand(0, 99999), 5, '0', STR_PAD_LEFT);
+            $sqlCheck = "SELECT COUNT(*) FROM ie WHERE codie = :codie";
+            $resultCheck = $conexion->prepare($sqlCheck);
+            $resultCheck->bindParam(":codie", $codie);
+            $resultCheck->execute();
+            $exists = $resultCheck->fetchColumn();
+        } while ($exists > 0);
+        try {
+            // Insertar el registro
+            $sql = "INSERT INTO ie  (nomie, tipie, dirie, nuicie, corie, codubi, telie, actie) VALUES (:nomie, :tipie, :dirie, :nuicie, :corie, :codubi, :telie, :actie)";
+            $result = $conexion->prepare($sql);
+            $nomie = $this->getNomie();
+            $result->bindParam(":nomie", $nomie);
+            $tipie = $this->getTipie();
+            $result->bindParam(":tipie", $tipie);
+            $dirie = $this->getDirie();
+            $result->bindParam(":dirie", $dirie);
+            $nuicie = $this->getNuicie();
+            $result->bindParam(":nuicie", $nuicie);
+            $corie = $this->getCorie();
+            $result->bindParam(":corie", $corie);
+            $codubi = $this->getCodubi();
+            $result->bindParam(":codubi", $codubi);
+            $telie = $this->getTelie();
+            $result->bindParam(":telie", $telie);
+            $actie = $this->getActie();
+            $result->bindParam(":actie", $actie);
+            if ($result->execute()) {
+                echo "<script>alert('Institución creada exitosamente.');</script>";
+            }
+        } catch (PDOException $e) {
+            echo "<script>alert('Error al guardar la Institución. " . addslashes($e->getMessage()) . "');</script>";
+        }
+    }
 
-  // Métodos Setter
-  public function setNuicie($nuicie) {
-    $this->nuicie = $nuicie;
-  }
+    public function edit()
+    {
+        $modelo = new Conexion();
+        $conexion = $modelo->get_conexion();
+        $sql = "UPDATE ie SET SELECT nomie=:nomie, tipie=:tipie, dirie=:dirie, nuicie=:nuicie, corie=:corie, codubi=:codubi, telie=:telie, actie=:actie WHERE codie=:codie";
+        $result = $conexion->prepare($sql);
+        $codie = $this->getCodie();
+        $result->bindParam(":codie", $codie);
+        $nomie = $this->getNomie();
+        $result->bindParam(":nomie", $nomie);
+        $tipie = $this->getTipie();
+        $result->bindParam(":tipie", $tipie);
+        $dirie = $this->getDirie();
+        $result->bindParam(":dirie", $dirie);
+        $nuicie = $this->getNuicie();
+        $result->bindParam(":nuicie", $nuicie);
+        $corie = $this->getCorie();
+        $result->bindParam(":corie", $corie);
+        $codubi = $this->getCodubi();
+        $result->bindParam(":codubi", $codubi);
+        $telie = $this->getTelie();
+        $result->bindParam(":telie", $telie);
+        $actie = $this->getActie();
+        $result->bindParam(":actie", $actie);
+        try {
+            if ($result->execute()) {
+                echo "<script>alert('Institución actualizada exitosamente.');</script>";
+            }
+        } catch (PDOException $e) {
+            echo "<script>alert('Error al actualizar la Institución. " . addslashes($e->getMessage()) . "');</script>";
+        }
+    }
+    function ediActie(){
+        $sql = "UPDATE ie SET actie=:actie WHERE codie=:codie";
+        $modelo = new conexion();
+        $conexion = $modelo->get_conexion();
+        $result = $conexion->prepare($sql);
+        $codie = $this->getCodie();
+        $result->bindParam(":codie",$codie);
+        $actie = $this->getActie();
+        $result->bindParam(":actie",$actie);
+        $result->execute(); 
+    }
+    public function del()
+    {
+        $sql = "DELETE FROM ie WHERE codie= :codie";
+        $modelo = new Conexion();
+        $conexion = $modelo->get_conexion();
+       
+        $result = $conexion->prepare($sql);
+        $codie = $this->getCodie();
+        $result->bindParam(":codie", $codie);
 
-  public function setDepubi($depubi) {
-    $this->depubi = $depubi;
-  }
-
-  public function setMunubi($munubi) {
-    $this->munubi = $munubi;
-  }
-
-  public function setCorie($corie) {
-    $this->corie = $corie;
-  }
-
-  public function setTelie($telie) {
-    $this->telie = $telie;
-  }
-
-  // Método para obtener todas las instituciones
-  public function getAll() {
-    $res = NULL;
-    $sql = "SELECT * FROM ie";
-    $modelo = new Conexion();
-    $conexion = $modelo->get_conexion();
-    $result = $conexion->prepare($sql);
-    $result->execute();
-    $res = $result->fetchAll(PDO::FETCH_ASSOC);
-    return $res;
-  }
-
-  // Método para obtener una institución
-  public function getOne() {
-    $res = NULL;
-    $sql = "SELECT * FROM ie WHERE nuicie=:nuicie";
-    $modelo = new Conexion();
-    $conexion = $modelo->get_conexion();
-    $result = $conexion->prepare($sql);
-    $nuicie = $this->getNuicie();
-    $result->bindParam(":nuicie", $nuicie);
-    $result->execute();
-    $res = $result->fetchAll(PDO::FETCH_ASSOC);
-    return $res;
-  }
-
-  // Método para guardar una institución
-  public function save() {
-    $sql = "INSERT INTO ie (nuicie, depubi, munubi, corie, telie) VALUES (:nuicie, :depubi, :munubi, :corie, :telie)";
-    $modelo = new Conexion();
-    $conexion = $modelo->get_conexion();
-    $result = $conexion->prepare($sql);
-    $nuicie = $this->getNuicie();
-    $result->bindParam(":nuicie", $nuicie);
-    $depubi = $this->getDepubi();
-    $result->bindParam(":depubi", $depubi);
-    $munubi = $this->getMunubi();
-    $result->bindParam(":munubi", $munubi);
-    $corie = $this->getCorie();
-    $result->bindParam(":corie", $corie);
-    $telie = $this->getTelie();
-    $result->bindParam(":telie", $telie);
-    $result->execute();
-  }
-
-  // Método para editar una institución
-  public function edit() {
-    $sql = "UPDATE ie SET depubi=:depubi, munubi=:munubi, corie=:corie, telie=:telie WHERE nuicie=:nuicie";
-    $modelo = new Conexion();
-    $conexion = $modelo->get_conexion();
-    $result = $conexion->prepare($sql);
-    $nuicie = $this->getNuicie();
-    $result->bindParam(":nuicie", $nuicie);
-    $depubi = $this->getDepubi();
-    $result->bindParam(":depubi", $depubi);
-    $munubi = $this->getMunubi();
-    $result->bindParam(":munubi", $munubi);
-    $corie = $this->getCorie();
-    $result->bindParam(":corie", $corie);
-    $telie = $this->getTelie();
-    $result->bindParam(":telie", $telie);
-    $result->execute();
-  }
-
-  // Método para eliminar una institución
-  public function del() {
-    $sql = "DELETE FROM ie WHERE nuicie=:nuicie";
-    $modelo = new Conexion();
-    $conexion = $modelo->get_conexion();
-    $result = $conexion->prepare($sql);
-    $nuicie = $this->getNuicie();
-    $result->bindParam(":nuicie", $nuicie);
-    $result->execute();
-  }
+        try {
+            if ($result->execute()) {
+                echo "<script>alert('Fase eliminada exitosamente.');</script>";
+            }
+        } catch (PDOException $e) {
+            echo "<script>alert('Error al eliminar la Institución. " . addslashes($e->getMessage()) . "');</script>";
+        }
+    }
 }
 ?>
