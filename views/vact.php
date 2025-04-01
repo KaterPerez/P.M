7c6a93b4618112a21be19f70b47233e89e9060c7
WagnerA0 <alejo.wagner03@gmail.com>
2025-03-31 22:29:15 -0500

    Update vact.php

--- views/vact.php
+++ views/vact.php
@@ -1,4 +1,4 @@
-<?php include("controllers/cact.php")?>
+<?php include("controllers/cact.php") ?>
 
 <div class="container">
 
@@ -17,45 +17,45 @@
         style="<?= isset($m) && $m == 1 ? '' : 'display:none;' ?>">
         <div class="row">
             <div class="form-group col-md-4">
-                <label for="nomfas">Nombre de la actividad</label>
-                <input type="text" class="form-control form-control" name="nomfas" id="nomfas" value="<?php if ($datOne && $datOne[0]['nomfas'])
-                    echo $datOne[0]['nomfas']; ?>" required>
+                <label for="nomact">Nombre de la actividad</label>
+                <input type="text" class="form-control form-control" name="nomact" id="nomact" value="<?php if ($datOne && $datOne[0]['nomact'])
+                    echo $datOne[0]['nomact']; ?>" required>
             </div>
             <div class="form-group col-md-5">
-                <label for="despro">Descripción del proyecto</label>
-                <input type="text" class="form-control form-control" name="despro" id="despro" value="<?php if ($datOne && $datOne[0]['despro'])
-                    echo $datOne[0]['despro']; ?>" required>
+                <label for="desact">Descripción de la actividad</label>
+                <input type="text" class="form-control form-control" name="desact" id="desact" value="<?php if ($datOne && $datOne[0]['desact'])
+                    echo $datOne[0]['desact']; ?>" required>
             </div>
             <div class="form-group col-md-3">
                 <label for="disabledSelect" class="form-label">Fase</label>
-                <select class="form-control form-select" id="codpro" name="codpro">
+                <select class="form-control form-select" id="codfas" name="codfas">
                     <option value="0">Seleccione...</option>
                     <?php if (!empty($cdpro)) {
                         foreach ($cdpro as $dt) { ?>
-                            <option value="<?= $dt['codpro']; ?>" <?php if ($datOne && $datOne[0]['codpro'] == $dt['codpro'])
-                                                                        echo 'selected'; ?>>
-                                <?= $dt['nompro']; ?>
+                            <option value="<?= $dt['codfas']; ?>" <?php if ($datOne && $datOne[0]['codfas'] == $dt['codfas'])
+                                  echo 'selected'; ?>>
+                                <?= $dt['nomfas']; ?>
                             </option>
-                    <?php }
+                        <?php }
                     } ?>
                 </select>
             </div>
             <div class="form-group col-md-4">
-                <label for="inifas">Fecha de inicio</label>
-                <input type="date" class="form-control form-control" name="inifas" id="inifas" value="<?php if ($datOne && $datOne[0]['inifas'])
-                                                                                                            echo $datOne[0]['inifas']; ?>" required>
+                <label for="iniact">Fecha de inicio</label>
+                <input type="date" class="form-control form-control" name="iniact" id="iniiniactas" value="<?php if ($datOne && $datOne[0]['iniact'])
+                    echo $datOne[0]['iniact']; ?>" required>
             </div>
             <div class="form-group col-md-4">
-                <label for="finfas">Fecha de finalizacion</label>
-                <input type="date" class="form-control form-control" name="finfas" id="finfas" value="<?php if ($datOne && $datOne[0]['finfas'])
-                                                                                                            echo $datOne[0]['finfas']; ?>" required>
+                <label for="finact">Fecha de finalizacion</label>
+                <input type="date" class="form-control form-control" name="finact" id="finact" value="<?php if ($datOne && $datOne[0]['finact'])
+                    echo $datOne[0]['finact']; ?>" required>
             </div>
 
             <div class="form-group col-md-4">
                 <br>
                 <input type="hidden" name="ope" value="save">
-                <input type="hidden" name="codfas" value="<?php if ($datOne && $datOne[0]['codfas'])
-                                                                echo $datOne[0]['codfas']; ?>" required>
+                <input type="hidden" name="codact" value="<?php if ($datOne && $datOne[0]['codact'])
+                    echo $datOne[0]['codact']; ?>" required>
                 <input type="submit" class="btn btn-dark" value="Enviar">
             </div>
 
@@ -66,28 +66,29 @@
     <table id="example" class="table table-striped text-center" style="width:100%">
         <thead class="table-dark">
             <tr>
+                <th class="text-center">Actividad</th>
+                <th class="text-center">Descripción</th>
                 <th class="text-center">Fase</th>
-                <th class="text-center">Proyecto</th>
-                <th class="text-center">Grupo</th>
                 <th class="text-center">Fecha de inicio</th>
                 <th class="text-center">Fecha de finalización</th>
-                <th class="text-center">Actividad</th>
-                <th class="text-center">Acciones</th>
+                <th class="text-center"></th>
+                <th class="text-center"></th>
             </tr>
         </thead>
         <tbody>
             <?php if ($datAll) {
                 foreach ($datAll as $dt) { ?>
                     <tr>
+                        <td class="text-center"><?= $dt["nomact"]; ?></td>
+                        <td class="text-center"><?= $dt["desact"]; ?></td>
                         <td class="text-center"><?= $dt["nomfas"]; ?></td>
-                        <td class="text-center"><?= $dt["nompro"]; ?></td> 
-                        <td class="text-center"><?= $dt["nomgru"]; ?></td>
-                        <td class="text-center"><?= $dt["inifas"]; ?></td>
-                        <td class="text-center"><?= $dt["finfas"]; ?></td>
-                        <td class="text-center">                            <!-- Botón para abrir el modal -->
+                        <td class="text-center"><?= $dt["iniact"]; ?></td>
+                        <td class="text-center"><?= $dt["finact"]; ?></td>
+                        <td class="text-center"> <!-- Botón para abrir el modal -->
                             <button>
                                 <div class="col-md-5">
-                                    <input type="file" name="archivo" id="archivo" accept=".pdf, .docx, .xlsx, .xls" class="form-control" style="width: 100%;">
+                                    <input type="file" name="archivo" id="archivo" accept=".pdf, .docx, .xlsx, .xls"
+                                        class="form-control" style="width: 100%;">
                                 </div>
                                 <div class="col-md-2">
                                     <button type="submit" class="btn btn-dark">Cargar Datos</button>
@@ -95,14 +96,15 @@
                             </button>
                         </td>
                         <td class="text-center">
-                            <a href="home.php?pg=3001&ope=del&codfas=<?= $dt["codfas"]; ?>" title="Eliminar"
-                                onclick="return confirm('¿Estás seguro de que deseas eliminar esta fase?');"><i
-                                    class="fa-solid fa-trash fa-2x text-danger" style="color: #000000;"></i></a>
-                            <a href="home.php?pg=3001&ope=edi&codfas=<?= $dt["codfas"]; ?>" title="Editar"><i
-                                    class="fa-solid fa-pen-to-square fa-2x text-success" style="color: #000000;"></i></a>
+                            <a href="home.php?pg=3002&ope=edi&codact=<?= $dt["codact"]; ?>" title="Editar">
+                                <i class="fa-solid fa-pen-to-square fa-2x text-success" style="color: #000000;"></i>
+                            </a>
+                            <a href="home.php?pg=3002&ope=del&codact=<?= $dt["codact"]; ?>" title="Eliminar"
+                                onclick="return confirm('¿Estás seguro de que deseas eliminar esta Actividad?');"><i class="fa-solid fa-trash fa-2x text-danger" style="color: #000000;"></i>
+                            </a>
                         </td>
                     </tr>
-            <?php }
+                <?php }
             } ?>
         </tbody>
     </table>
