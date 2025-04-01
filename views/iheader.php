f03ef7e07cb2fed455a2d9c85125c7748cfaa036
Ayala065 <juan.ayala0565@gmail.com>
2025-03-31 21:53:59 -0500

    Update iheader.php

--- views/iheader.php
+++ views/iheader.php
@@ -1,4 +1,4 @@
-<div class="container ">
+<div class="container">
     <nav class="navbar navbar-expand-lg">
         <img src="img/PM-removebg-preview.jpg" width="250px" height="100px">
         <div class="navbar-collapse">
@@ -7,7 +7,58 @@
                 <li class="nav-item"><a class="nav-link" href="index.php?pg=1896">Precio</a></li>
             </ul>
         </div>
-        <a href="index.php?pg=300"><button type="button" class="btn btn-dark" >Iniciar sesión</button></a>
+        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#loginModal">
+            Iniciar sesión
+        </button>
     </nav> 
-</div>
+</div>  
 
+<div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content shadow-lg rounded-4">
+            <div class="modal-header bg-dark text-white">
+                <h5 class="modal-title w-100 text-center" id="loginModalLabel">Iniciar Sesión</h5>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+            </div>
+            <div class="modal-body">
+                <form action="models/validacion.php" method="POST">
+                    <div class="mb-3">
+                        <label for="usu" class="form-label">Correo Electrónico</label>
+                        <div class="input-group">
+                            <span class="input-group-text"><i class="fa-solid fa-envelope"></i></span>
+                            <input type="email" class="form-control" name="usu" id="usu" placeholder="Correo Electrónico" required>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="con" class="form-label">Contraseña</label>
+                        <div class="input-group">
+                            <span class="input-group-text"><i class="fa-solid fa-lock"></i></span>
+                            <input type="password" class="form-control" name="con" id="con" placeholder="Contraseña" required>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <button type="submit" class="btn btn-dark w-100">Ingresar</button>
+                        </div>
+                        <div class="col-6">
+                            <a href="index.php?pg=205" class="btn btn-outline-dark w-100 text-center">Olvidó su contraseña</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+<style>
+    .btn-close-white {
+        background-color: transparent;
+        box-shadow: none;
+    }
+    .modal .btn-close-white:hover {
+        background-color: white !important; 
+        color: black !important; 
+        border: 1px solid black; 
+    }
+</style>
